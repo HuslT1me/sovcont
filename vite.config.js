b0ec08090d037d88f43d64c564904ec8bda3f6ac
Vadim <husltime09@gmail.com>
2025-08-10 16:44:38 +0300

    fixes routs

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/sovcont/",
+  base: "./",
   publicDir: "public",
   resolve: {
     alias: {
@@ -14,13 +14,22 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "main.html"),
+        index: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        cases_item: resolve(__dirname, "cases-item.html"),
+        cases: resolve(__dirname, "cases.html"),
+        contacts: resolve(__dirname, "contacts.html"),
+        news_item: resolve(__dirname, "news-item.html"),
+        news: resolve(__dirname, "news.html"),
+        partners: resolve(__dirname, "partners.html"),
+        services_alt: resolve(__dirname, "services_alt.html"),
+        services: resolve(__dirname, "services.html"),
       },
     },
   },
   server: {
     port: 3000,
-    open: '/sovcont/main.html',
+    open: '/index.html',
   },
   css: {
     preprocessorOptions: {
